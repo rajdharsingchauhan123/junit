1a94544f663b2dc48592575d57d1d360ba15a596
raj <raj@gmail.com>
2023-03-03 17:38:20 +0530

    assertTure method

---
+++ src/main/java/org/example/StingUtilites.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StingUtilites {
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,12 +1,28 @@
+import org.example.StingUtilites;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Demo {
+
     @Test
     public void methodTesting(){
        // assertEquals(2,3);
         assertNotEquals(1,9);
+    }
+    @Test
+  public void test1(){
+        assertTrue(true);
+
+    }
+    @Test
+    public void test2(){
+        assertTrue(2==2);
+
+    }
+    @Test
+    public void test3(){
+        assertTrue("abc".length()==3);
+
     }
 }
