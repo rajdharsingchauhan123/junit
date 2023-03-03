2470c4158adcce5c8634d03c1a91e6259de2e9f5
raj <raj@gmail.com>
2023-03-03 17:25:40 +0530

    for assertion pass

---
+++ src/test/java/Demo.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Demo {
+    @Test
+    public void methodTesting(){
+        assertEquals(2,2);
+    }
+}
