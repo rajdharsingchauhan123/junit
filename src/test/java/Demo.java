8bd4c72b42cefc5dda2daf726baf6b55c0d265d5
raj <raj@gmail.com>
2023-03-03 17:29:48 +0530

    assertnotequalmethod

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class Demo {
     @Test
     public void methodTesting(){
-        assertEquals(2,3);
+       // assertEquals(2,3);
+        assertNotEquals(1,9);
     }
 }
