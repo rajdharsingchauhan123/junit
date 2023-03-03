356bccc450a7310c8ec6401457dc53190de7c134
raj <raj@gmail.com>
2023-03-03 18:27:18 +0530

    using before all and afterall

--- src/test/java/ReactangleUtilities.java
+++ src/test/java/ReactangleUtilities.java
@@ -1,9 +1,20 @@
 import org.example.ReactangelUtilities;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReactangleUtilities {
+
+    @BeforeAll
+    public static void setup(){
+        System.out.println("first");
+    }
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("last");
+    }
     @Test
     public void tesGetParameter(){
         int height=4;
